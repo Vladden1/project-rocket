ec8c5b3b67626859c983fa193591c3965a613262
Vladden1 <103289443+Vladden1@users.noreply.github.com>
2022-09-26 11:30:43 +0000

    Deploying to gh-pages from @ Vladden1/project-rocket@8daa9e238682f0fcc04a00050486b5888554d2fc 🚀

---
+++ index.00b6b10a.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-video__modal-open]"),closeModalBtn:document.querySelector("[data-video__modal-close]"),modal:document.querySelector("[data-video__modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.00b6b10a.js.map
