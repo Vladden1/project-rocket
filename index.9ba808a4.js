195dac1137e97d611bbe0b1ae070441f1913f1b4
Vladden1 <103289443+Vladden1@users.noreply.github.com>
2022-09-27 07:41:25 +0000

    Deploying to gh-pages from @ Vladden1/project-rocket@af68356933ce1a04bb863ad47fb576f513bfd449 🚀

---
+++ index.9ba808a4.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".swiper",{direction:"horizontal",loop:!0,pagination:{el:".swiper-pagination",clickable:!0},scrollbar:{el:".swiper-scrollbar"},grabCursor:!0,keyboard:{enabled:!0,onlyInViewport:!0,pageUpDown:!0}});
+//# sourceMappingURL=index.9ba808a4.js.map
--- index.d561201b.js
+++
@@ -1,2 +0,0 @@
-var swiper=new Swiper(".swiper",{direction:"horizontal",loop:!0,pagination:{el:".swiper-pagination",clickable:!0,dynamicBullets:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},scrollbar:{el:".swiper-scrollbar"},grabCursor:!0,keyboard:{enabled:!0,onlyInViewport:!0,pageUpDown:!0}});
-//# sourceMappingURL=index.d561201b.js.map
